3f02402e993e5a6a3ea87db9510c283cbfbeb1b2
Savaş Erbas <savas.erbas@mondas-gmbh.de>
2020-06-29 21:27:59 +0200

    rezept project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,10 @@ import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { OtherComponent } from "./other/other.component";
-import { DatabindingComponent } from "./databinding/databinding.component";
-import { PropertyBindingComponent } from "./databinding/property-binding.component";
-import { EventBindingComponent } from "./databinding/event-binding.component";
-import { UseHTMLelementsComponent } from './use-htmlelements.component';
-import { LifecycleComponent } from './lifecycle/lifecycle.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    OtherComponent,
-    DatabindingComponent,
-    PropertyBindingComponent,
-    EventBindingComponent,
-    UseHTMLelementsComponent,
-    LifecycleComponent,
-  ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
